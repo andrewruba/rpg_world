09687b45dfef2397795f62cf4ea048a040372bc9
Andrew Ruba <andrewruba@Andrews-MBP.fios-router.home>
2024-09-12 18:52:29 -0400

    basic framework

---
+++ src/rpg_simulator/__init__.py
@@ -0,0 +1,16 @@
+# rpg_simulation/__init__.py
+
+# Version of the package
+__version__ = '0.1.0'
+
+# Exposing core classes and functions
+from .character.base_character import BaseCharacter
+# from .character.player_character import PlayerCharacter
+# from .character.npc_character import NPCCharacter
+# from .abilities.spells import Spell
+# from .combat.combat_system import CombatSystem
+# from .items.weapon import Weapon
+# from .items.consumable import Consumable
+# from .config import game_config
+
+# Any other core imports or package-wide initialization can go here.
---
+++ src/rpg_simulator/character/__init__.py
@@ -0,0 +1,8 @@
+# rpg_simulation/character/__init__.py
+
+from .base_character import BaseCharacter
+# from .player_character import PlayerCharacter
+# from .npc_character import NPCCharacter
+
+# By including this, users can import characters like this:
+# from rpg_simulation.character import PlayerCharacter
