1e204c99caa24290fdab423db7a757b7383307cb
Andrew Ruba <andrewruba@Andrews-MBP.fios-router.home>
2024-09-21 17:50:49 -0400

    update env and reqs and req script

--- scripts/update_reqs.sh
+++ scripts/update_reqs.sh
@@ -3,8 +3,8 @@
 # This script updates the requirements.txt file by commenting out specific package versions
 # and appending a custom wheel file reference.
 # Usage:
-# 1. Ensure the script is executable: chmod +x scripts/update_requirements.sh
-# 2. Run the script: ./scripts/update_requirements.sh
+# 1. Ensure the script is executable: chmod +x scripts/update_reqs.sh
+# 2. Run the script: ./scripts/update_reqs.sh
 
 # Generate requirements.txt
 pip list --format=freeze > requirements.txt
@@ -21,7 +21,7 @@ sed -i '' 's/^mkl-service==2.4.0/# &/' $temp_file
 # sed -i '' 's/^gmpy2==2.1.2/# &/' $temp_file
 
 # Delete specific lines from the temporary file
-sed -i '' '/^rpg_world==1.0.0/d' $temp_file
+sed -i '' '/^rpg_world/d' $temp_file
 
 # Append the custom wheel file reference for mdurl
 # echo './wheels/mdurl-0.1.2-py3-none-any.whl' >> $temp_file
